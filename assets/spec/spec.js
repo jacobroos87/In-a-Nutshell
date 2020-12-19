f0487ab1ac95feb7af2e485d8e23add9dfac559f
jacobroos87 <jacobroos87@gmail.com>
2020-12-19 17:28:36 +0000

    Added some simple jasmine tests

--- assets/spec/spec.js
+++ assets/spec/spec.js
@@ -1,12 +1,18 @@
 describe("Form submission test", () => {
-    
+    $('#contact-form').on('submit', function () {
+        return false;
+    });
     it("Should trigger the modal", () => {
-        var spyEvent;
-        spyEvent = spyOn('#mySuccessModal', show());
-    $("#contact-form").trigger( "submit" );
-       
-    expect(show()).toHaveBeenTriggeredOn('#contact-form');
-    expect(spyEvent).toHaveBeenTriggered();
 
+        $("#contact-form").trigger("submit");
+        expect($(".modal-body h4").html()).toBe("Your form submission was a SUCCESS!!")
+    })
+})
+
+describe("Nut selector function tests", () => {
+    it("Should make a call to the FDC api", () => {
+        $(".nut-click-icon").trigger("click");
+        
+    expect($("#nut-data, #joke-generator, #contact-form-section").show().fadeIn()).toBeTruthy();
     })
 })
